f5ab7cfab82bc19589c757fa6aeb7b34916b89f9
pablo <pablolafontaine1@gmail.com>
2024-01-03 12:45:43 -0500

    Added content

--- src/components/Index.js
+++ src/components/Index.js
@@ -24,7 +24,10 @@ export default async function Index() {
         </a>
       </div>
       <main className="py-2">
-        <p>welcome to my site and blog!</p>
+        <p>welcome to my site and blog! <br />
+	  currently, i{"'"}m a 4th year computer science student. here, you can find blog posts where i{"'"}ll mainly focus on rust, golang, unix, vim, and projects i{"'"}m working on. i{"'"}ll also be using it to talk about other interests i have, such as video games, books, and general life stuff.<br />
+	  previously, i{"'"}ve been a part of the mlh fellowship program; working with castle finance on their solana-powered web3 vaults. i{"'"}ve also worked as a teaching assistant at my university.</p>
+
       </main>
       <Divider className="py-2" />
       <p className="text-2xl pb-2 pt-2"> Posts </p>
